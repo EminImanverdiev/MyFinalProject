e6352b3c454ffb06821cdc54b874b4d8dcbc5707
elmin007 <imanverdiyevemin414@gmail.com>
2024-04-23 00:56:29 +0400

    Added WebAPI Project to my Project

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -33,7 +33,7 @@ namespace Business.Concrete
 
 		public IDataResult<List<Product>> GetAll()
 		{
-            if (DateTime.Now.Hour==17)
+            if (DateTime.Now.Hour==12)
             {
 				return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
                 
